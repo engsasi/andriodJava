f0e52a03ecb01540ae4630c20405b7f3c17435fa
unknown <sasikan.mu@ku.th>
2023-02-22 00:08:44 +0700

    first commit

---
+++ app/src/main/java/com/example/projects/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.projects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView nextregister = (TextView) findViewById(R.id.nextregister);
+        nextregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/projects/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.projects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        TextView nextlogin= (TextView) findViewById(R.id.nextlogin);
+        nextlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
